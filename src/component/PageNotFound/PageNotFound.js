40d34b9c3970a9d978bacec48d5865cee69a5690
Saima <saimasawrin324@gmail.com>
2022-11-23 23:22:14 +0600

    404 page added

---
+++ src/component/PageNotFound/PageNotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './page.css';
+const PageNotFound = () => {
+    return (
+  
+       <div className='error'>
+        <a href="http://localhost:3000/"><img
+           src="/404.jpg"
+          alt="React Bootstrap logo" /></a>
+
+        </div>
+    
+     
+    );
+};
+
+export default PageNotFound;
